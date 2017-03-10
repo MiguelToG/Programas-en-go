d9d6dbf7104bab492af606ec8a50ed13dd86944f
Miguel Flores <rmiguel.0.0.77@gmail.com>
2017-03-10 15:27:42 -0600

    Go web
    Primer hola mundo web en go

---
+++ hola_web.go
@@ -0,0 +1,17 @@
+package main 
+import (
+"net/http"
+"io"
+"fmt"
+)
+func main() {
+	http.HandleFunc("/holamundo",func(w http.ResponseWriter,r *http.Request){
+		io.WriteString(w,"Hollaaaaa")
+		})
+	http.HandleFunc("/",handler)
+	http.ListenAndServe(":8000",nil)
+}
+func handler(w http.ResponseWriter,r *http.Request){
+fmt.Println("Hay una nueva peticion")
+io.WriteString(w,"Hola mundo")
+}
